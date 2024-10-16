8a8e0ca5b5b841616429a01e82835ff944414725
chetankhandait <chetankhandait16@gmail.com>
2024-10-16 21:32:25 +0530

    change all the extension

--- src/app/api/classes/route.ts
+++ src/app/api/classes/route.ts
@@ -1,19 +1,31 @@
-// src/app/api/classes/route.js
-import { supabase } from '@/lib/supabaseClient';
+// src/app/api/classes/route.ts
 
-export async function GET() {
+import { supabase } from "@/app/libs/supabaseClient";
+
+ 
+interface DanceClass {
+  teacher: string;
+  danceForm: string;
+  timing: string;
+  date: string;
+}
+
+export async function GET(): Promise<Response> {
   const { data, error } = await supabase.from('Classes').select('*');
+  
   if (error) {
     return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
     });
   }
+  
   return new Response(JSON.stringify(data), { status: 200 });
 }
 
-export async function POST(request) {
-  const body = await request.json();
+export async function POST(request: Request): Promise<Response> {
+  const body: DanceClass = await request.json();
   const { teacher, danceForm, timing, date } = body;
+  
   const { data, error } = await supabase.from('Classes').insert([{ teacher, danceForm, timing, date }]);
   
   if (error) {
@@ -21,5 +33,6 @@ export async function POST(request) {
       status: 500,
     });
   }
+  
   return new Response(JSON.stringify(data), { status: 201 });
 }
